3d0b478179d9f3b9ff75d1780dc1ffef407699dc
Jie Chen <jiechenmc@gmail.com>
2024-06-06 00:18:16 -0400

    setup minecraft

---
+++ mc.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "minecraft" {
+  ingress {
+    description = "Receive SSH from home."
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Receive Minecraft from everywhere."
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Send everywhere."
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Minecraft"
+  }
+}
+
+resource "aws_key_pair" "home" {
+  key_name   = "Home"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "minecraft" {
+  ami                         = "ami-04b70fa74e45c3917"
+  instance_type               = "t2.medium"
+  vpc_security_group_ids      = [aws_security_group.minecraft.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.home.key_name
+  tags = {
+    Name = "Minecraft"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.minecraft.public_ip
+}
